a0d7646266d5d73943fdf6be2caf35673e4e02ba
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-10-20 11:01:34 +0300

    2

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,7 +1,7 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
 let age: number = 50;
-let name = 'Max';
+let name: string = 'Max';
 let toggle = true;
 let empty = null;
 let notInitialize;
